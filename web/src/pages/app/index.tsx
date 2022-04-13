260e10108c66823fa05fd372219fa15317df5b6f
allexis <allexissoares@gmail.com>
2022-04-12 21:23:38 -0300

    feat: improve the frontend using graphql connection with apollo and codegen

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,16 +1,28 @@
-import { getSession, useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { GetServerSideProps } from 'next'
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { useMeQuery } from '../../graphql/generated/graphql'
+import {
+  getServerPageGetProducts,
+  ssrGetProducts,
+} from '../../graphql/generated/page'
+import { withApollo } from '../../lib/withApollo'
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser()
+  const { data: me } = useMeQuery()
 
   return (
-    <div>
+    <div className="text-violet-500">
       <pre>{JSON.stringify(user, null, 2)}</pre>
-
-      <a href="/api/auth/logout">Logout</a>
+      <pre>{JSON.stringify(data.products, null, 2)}</pre>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
     </div>
   )
 }
 
-export const getServerSideProps = withPageAuthRequired()
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {
+    return getServerPageGetProducts(null, ctx)
+  },
+})
+
+export default withApollo(ssrGetProducts.withPage()(Home))
